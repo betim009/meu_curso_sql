c409201476a08f65cdc079a013625c4d26caeaad
Alberto <betim009@gmail.com>
2026-04-15 20:35:20 -0300

    update

--- aula/aula1.sql
+++ aula/aula1.sql
@@ -1,1 +1,6 @@
 -- Um banco de dados é como uma “pasta” onde você guarda suas tabelas.
+
+-- exemplo: CREATE DATABASE nome_do_banco;
+
+-- Usar o banco
+-- Depois de criar, você precisa “entrar” nele:
