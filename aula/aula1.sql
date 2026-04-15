4649caaf7ea40ca499a170ad05fc793636244966
Alberto <betim009@gmail.com>
2026-04-15 20:36:46 -0300

    update

--- aula/aula1.sql
+++ aula/aula1.sql
@@ -4,3 +4,13 @@
 
 -- Usar o banco
 -- Depois de criar, você precisa “entrar” nele:
+
+-- Exercício 2 (nível um pouco maior)
+-- Agora quero ver se você realmente entendeu.
+-- 👉 Faça duas coisas:
+-- Crie um banco chamado:
+-- loja
+-- Entre (use) esse banco
+
+CREATE DATABASE loja;
+use loja;
